dbaf2b16f417ddf4ce90b5208ed58133c1c7ce11
sakkoumhamza <hamzasakkoum1@gmail.com>
2025-09-13 15:29:23 +0100

    Added a test script

--- test_main.py
+++ test_main.py
@@ -10,14 +10,14 @@ class TestJSONLoaderMethods(unittest.TestCase):
         with open('books.json') as json_file:
             cls.books = json.load(json_file)
 
-    def test_rank(self):
+    def test_author(self):
         self.assertEqual(self.books[0]['author'], 'Daniel Defoe')
     
     def test_title(self):
         self.assertEqual(self.books[0]['title'], 'Robinson Crusoe')
     
     def test_id(self):
-        self.assertEqual(self.books[0]['id'], '1')
+        self.assertEqual(self.books[0]['id'], '0')
 
 if __name__ == '__main__':
     unittest.main()
